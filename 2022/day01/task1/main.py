4f5417b3655e2f6c20c74c1bd3d63f3deeac465f
John Pullmann <john.pullmann@gmail.com>
2022-12-13 15:34:57 +0100

    Updata Day01-Task1

--- 2022/day01/task1/main.py
+++ 2022/day01/task1/main.py
@@ -1,8 +1,11 @@
+import os
 
 def load_input() -> list[str]:
     # Read and return text input from input.txt
     try:
-        with open("input.txt", 'r') as file:
+        dirname = os.path.dirname(__file__)
+        path_to_input = os.path.join(dirname, 'input.txt')
+        with open(path_to_input, 'r') as file:
             inp = file.read().splitlines()
     except FileNotFoundError:
         print("File input.txt was not found!")
@@ -36,3 +39,4 @@ if __name__ == "__main__":
     inp = load_input()
     if inp != -1:
         result = get_max_cal(inp)
+    print(result)
