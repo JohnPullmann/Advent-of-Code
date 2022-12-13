2a8d8de39d8654856b511b20465e74ec22fdd5be
John Pullmann <john.pullmann@gmail.com>
2022-12-13 15:40:25 +0100

    Add Day01-Task2

---
+++ 2022/day01/task2/main.py
@@ -0,0 +1,46 @@
+import os
+
+def load_input() -> list[str]:
+    # Read and return text input from input.txt
+    try:
+        dirname = os.path.dirname(__file__)
+        path_to_input = os.path.join(dirname, 'input.txt')
+        with open(path_to_input, 'r') as file:
+            inp = file.read().splitlines()
+    except FileNotFoundError:
+        print("File input.txt was not found!")
+        return -1
+    return inp
+
+
+def get_max_cal(inp: list = []) -> int:
+    # sum calories for each elf and find elf with most calories
+    elfs_cal = []
+    elf_cal = 0
+    for line in inp:
+        if line == '':
+            elfs_cal.append(elf_cal)
+            elf_cal = 0
+        else:
+            try:
+                elf_cal += int(line)
+            except ValueError:
+                print("Invalid input!")
+                return -1
+
+    if elfs_cal != []:
+        elfs_cal.sort(reverse=True)
+        if len(elfs_cal) > 3:
+            return sum([elfs_cal[0],elfs_cal[1],elfs_cal[2]])
+        else:
+            return sum(elfs_cal)
+    else:
+        print("Input is empty!")
+        return -1
+
+
+if __name__ == "__main__":
+    inp = load_input()
+    if inp != -1:
+        result = get_max_cal(inp)
+    print(result)
