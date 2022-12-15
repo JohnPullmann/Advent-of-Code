421a66989c466327ef0ab5be3f3928199c3fac86
John Pullmann <john.pullmann@gmail.com>
2022-12-15 16:06:22 +0100

    Add Day06-Task2

---
+++ 2022/day06/task2/main.py
@@ -0,0 +1,32 @@
+import os
+
+def load_input() -> list[str]:
+    """read input.txt file for task input and return list of lines of input"""
+    # Read and return text input from input.txt
+    try:
+        dirname = os.path.dirname(__file__)
+        path_to_input = os.path.join(dirname, 'input.txt')
+        with open(path_to_input, 'r') as file:
+            inp = file.read().splitlines()
+    except FileNotFoundError:
+        print("File input.txt was not found!")
+        return -1
+    return inp
+
+
+def tuning_trouble(inp: list = [str]) -> int:
+    """find first sequence of 14 unique characters in a row and return index of sequence"""
+    sequence = ""
+    for i, char in enumerate(inp[0]):
+        sequence += char
+        if len(sequence) > 14:
+            sequence = sequence[1:]
+            if len(set(sequence)) == 14:
+                return i+1
+
+
+if __name__ == "__main__":
+    inp = load_input()
+    if inp != -1:
+        result = tuning_trouble(inp)
+    print(result)
